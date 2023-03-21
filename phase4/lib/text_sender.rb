b80e9893d0072a0380479300e9f49e8193c732a9
umut <umut_baykan@hotmail.com>
2023-03-21 15:08:59 +0000

    twilio features unit testing

--- phase4/lib/text_sender.rb
+++ phase4/lib/text_sender.rb
@@ -1,20 +1,18 @@
 require 'twilio-ruby'
 
 class OrderSender
-  def message(order_message)
-    raise ArgumentError unless order_message.is_a? String != true
-    account_sid = ENV["TWILIO_SID"]
-    auth_token = ENV["TWILIO_AUTH"]
+  def initialize(client)
+    @client = client
+  end
+
+  def message(order_message=nil)
+    raise ArgumentError unless order_message
     twilio_sender = ENV["TWILIO_SENDER"]
     twilio_receiver = ENV["TWILIO_RECEIVER"]
-
-    @client = Twilio::REST::Client.new account_sid, auth_token
     message = @client.messages.create(
         body: "#{order_message}",
         to: "#{twilio_receiver}",
         from: "#{twilio_sender}")  
-
-    # puts message.sid
+    return "Message successfully sent!" if message
   end
 end
-
--- phase4/spec/text_sender_spec.rb
+++ phase4/spec/text_sender_spec.rb
@@ -1,14 +1,26 @@
 require 'text_sender'
 
 describe OrderSender do
-  # it "sends a message to the user with the total price and estimated date" do
-  #   order_message = "Your basket with a total cost of £9 will be delivered by 18:30"
-  #   message_sender = OrderSender.new
-  #   expect(message_sender.message(order_message)).to eq "Message successfully sent!"
-  # end
+  account_sid = ENV["TWILIO_SID"]
+  auth_token = ENV["TWILIO_AUTH"]
+  client = Twilio::REST::Client.new(account_sid, auth_token)
+  order_message = "Your basket with a total cost of £9 will be delivered by 18:30"
+  params = {body: "#{order_message}", to: ENV["TWILIO_RECEIVER"], from: ENV["TWILIO_SENDER"]}
+
+  xit "sends a message to the user with the total price and estimated date" do
+    message_sender = OrderSender.new(client)
+    expect(message_sender.message(order_message)).to eq "Message successfully sent!"
+  end
+
+  it "sends a message to the user with the total price and estimated date with mock client" do
+    mock_client = double(:mock_client)
+    message_sender = OrderSender.new(mock_client)
+    allow(mock_client).to receive_message_chain(:messages, :create).with(params).and_return("Object")
+    expect(message_sender.message(order_message)).to eq "Message successfully sent!"
+  end
 
   it "fails to send a message if order information is not passed in" do
-    message_sender = OrderSender.new
+    message_sender = OrderSender.new(client)
     expect{message_sender.message}.to raise_error(ArgumentError)
   end
 
