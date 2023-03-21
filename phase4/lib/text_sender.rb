ed7919809697c3c312e13d65262f2ef21fc8f36f
umut <umut_baykan@hotmail.com>
2023-03-21 15:39:47 +0000

    unit tests for twilio standalone class

--- phase4/lib/text_sender.rb
+++ phase4/lib/text_sender.rb
@@ -1,20 +1,24 @@
 require 'twilio-ruby'
 
 class OrderSender
+  def initialize(twilio_server=(Twilio::REST::Client.new(ENV["TWILIO_SID"], ENV["TWILIO_AUTH"])))
+    @twilio_server = twilio_server
+  end
+
   def message(order_message)
-    raise ArgumentError unless order_message.is_a? String != true
-    account_sid = ENV["TWILIO_SID"]
-    auth_token = ENV["TWILIO_AUTH"]
+    raise ArgumentError unless order_message
     twilio_sender = ENV["TWILIO_SENDER"]
     twilio_receiver = ENV["TWILIO_RECEIVER"]
-
-    @client = Twilio::REST::Client.new account_sid, auth_token
-    message = @client.messages.create(
+    message = @twilio_server.messages.create(
         body: "#{order_message}",
         to: "#{twilio_receiver}",
         from: "#{twilio_sender}")  
-
+    return "Message successfully sent!" if message
     # puts message.sid
   end
-end
 
+  def whoami
+    return @twilio_server
+  end
+
+end
--- phase4/spec/text_sender_spec.rb
+++ phase4/spec/text_sender_spec.rb
@@ -1,15 +1,25 @@
 require 'text_sender'
 
 describe OrderSender do
-  # it "sends a message to the user with the total price and estimated date" do
-  #   order_message = "Your basket with a total cost of £9 will be delivered by 18:30"
-  #   message_sender = OrderSender.new
-  #   expect(message_sender.message(order_message)).to eq "Message successfully sent!"
-  # end
+  order_message = "Your basket with a total cost of £9 will be delivered by 18:30"
+
+  ## this is the test that sends an actual message. xit it unless you want to spend credits
+  xit "sends a message to the user with the total price and estimated date" do
+    message_sender = OrderSender.new
+    expect(message_sender.message(order_message)).to eq "Message successfully sent!"
+  end
+  ##
+  it "sends a return message using a mock double" do
+    fake_client = double(:fake_client)
+    params = {body: "#{order_message}", to: ENV["TWILIO_RECEIVER"], from: ENV["TWILIO_SENDER"]}
+    allow(fake_client).to receive_message_chain(:messages, :create).with(params).and_return("Object verifier")
+    message_sender = OrderSender.new(fake_client)
+    expect(message_sender.message(order_message)).to eq "Message successfully sent!"
+    expect(fake_client.messages.create(params)).to eq "Object verifier"
+  end
 
   it "fails to send a message if order information is not passed in" do
     message_sender = OrderSender.new
     expect{message_sender.message}.to raise_error(ArgumentError)
   end
-
 end
