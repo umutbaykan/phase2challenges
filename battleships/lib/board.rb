c01d7e62e23a1f3720ee48fe8e02e28fb318f9de
umut <umut_baykan@hotmail.com>
2023-03-23 12:21:23 +0000

    ship placement methods and tests for board class

--- battleships/lib/board.rb
+++ battleships/lib/board.rb
@@ -13,7 +13,27 @@ class Board
     ship_length = params[:ship_length]
     ship_symbol = params[:ship_symbol]
     ship_orientation = params[:ship_orientation]
-    @board[row].map!.with_index {|char, index| (column...(column+ship_length)).include?(index) ? char = " #{ship_symbol} " : char }
+
+    if ship_orientation == "h"
+      @board[row].map!.with_index {|char, index| (column...(column+ship_length)).include?(index) ? char = " #{ship_symbol} " : char }
+    else
+      @board[row, ship_length].each {|row| row.map!.with_index {|char, index| column == index ? char = " #{ship_symbol} " : char } } 
+    end
+  end
+
+  def check_if_ship_position_suitable(params)
+    column = params[:column] - 1
+    row = params[:row] - 1
+    ship_length = params[:ship_length]
+    ship_orientation = params[:ship_orientation]
+    #Checking if the ship fits the board
+    if (column + ship_length) > @board[row].length && ship_orientation == "h"
+      return false
+    elsif (row + ship_length) > @board.length && ship_orientation == "v"
+      return false
+    #Checking if there are existing ships there
+    end
+    return true
   end
 
   def return_board_in_array
@@ -21,7 +41,6 @@ class Board
   end
 end
 
-board = Board.new(6)
-parameters = {column: 2, row: 2, ship_length: 3, ship_symbol: "C", ship_orientation: "h"}
-board.place_ship(parameters)
-board.show_board
+# board = Board.new(6)
+# parameters = {column: 4, row: 3, ship_length: 5, ship_symbol: "C", ship_orientation: "v"}
+# puts board.check_if_ship_position_suitable(parameters)
--- battleships/spec/board_spec.rb
+++ battleships/spec/board_spec.rb
@@ -2,9 +2,9 @@ require 'board'
 
 describe Board do
   context "it initializes to a square grid" do
-    board = Board.new(6)
     
     it "shows the board as a 2d array of dots" do
+      board = Board.new(6)
       expect(board.return_board_in_array).to eq [
         [" . "," . "," . "," . "," . "," . "],
         [" . "," . "," . "," . "," . "," . "],
@@ -15,9 +15,51 @@ describe Board do
       ]
     end
 
-    it "grabs the bit of row" do
+    it "places a ship in the given coordinates, horizontally" do
+      board = Board.new(6)
       parameters = {column: 2, row: 2, ship_length: 3, ship_symbol: "C", ship_orientation: "h"}
-      expect(board)
+      board.place_ship(parameters)
+      expect(board.return_board_in_array).to eq [
+        [" . "," . "," . "," . "," . "," . "],
+        [" . "," C "," C "," C "," . "," . "],
+        [" . "," . "," . "," . "," . "," . "],
+        [" . "," . "," . "," . "," . "," . "],
+        [" . "," . "," . "," . "," . "," . "],
+        [" . "," . "," . "," . "," . "," . "]
+      ]
+    end
+
+    it "places a ship in the given coordinates, vertically" do
+      board = Board.new(6)
+      parameters = {column: 2, row: 2, ship_length: 3, ship_symbol: "C", ship_orientation: "v"}
+      board.place_ship(parameters)
+      expect(board.return_board_in_array).to eq [
+        [" . ", " . ", " . ", " . ", " . ", " . "], 
+        [" . ", " C ", " . ", " . ", " . ", " . "], 
+        [" . ", " C ", " . ", " . ", " . ", " . "], 
+        [" . ", " C ", " . ", " . ", " . ", " . "], 
+        [" . ", " . ", " . ", " . ", " . ", " . "], 
+        [" . ", " . ", " . ", " . ", " . ", " . "]]
+    end
+
+    context "checks whether a ship fits within the board" do
+      board = Board.new(6)
+      it "returns true if the ship fits horizontally" do
+        parameters = {column: 2, row: 2, ship_length: 3, ship_orientation: "h"}
+        expect(board.check_if_ship_position_suitable(parameters)).to eq true
+      end
+      it "returns false if the ship is too large horizontally" do
+        parameters = {column: 5, row: 2, ship_length: 3, ship_orientation: "h"}
+        expect(board.check_if_ship_position_suitable(parameters)).to eq false
+      end
+      it "returns true if the ship fits vertically" do
+        parameters = {column: 2, row: 2, ship_length: 3, ship_orientation: "v"}
+        expect(board.check_if_ship_position_suitable(parameters)).to eq true
+      end
+      it "returns false if the ship is too large horizontally" do
+        parameters = {column: 2, row: 5, ship_length: 3, ship_orientation: "v"}
+        expect(board.check_if_ship_position_suitable(parameters)).to eq false
+      end
     end
   end
 end
